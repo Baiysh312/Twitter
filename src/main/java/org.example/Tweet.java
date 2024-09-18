b5df87103de333ddf09a63a022ca2f5b13e63059
Baiysh Mamatov <altynbekovich.98@gmail.com>
2024-09-18 19:02:20 +0600

    first commit

---
+++ src/main/java/org.example/Tweet.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Tweet {
+    private String content;
+    private User author;
+}
---
+++ src/main/java/org.example/User.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+
+public class User {
+    private String username;
+    private String fullName;
+    private String password;
+    List<Tweet> tweets;
+    List<User> followers;
+    List<User> following;
+}
